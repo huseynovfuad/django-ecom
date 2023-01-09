11328feb35b888e89b2811f79008c7544f888fa0
ASUS <fhuseynov803@gmail.com>
2023-01-09 18:33:33 +0400

    data

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 app_name = "accounts"
 
 urlpatterns = [
-    path("register/", views.register_view, name="register"),
-    path("logout/", views.logout_view, name="logout"),
+    # path("register/", views.register_view, name="register"),
+    # path("logout/", views.logout_view, name="logout"),
 ]
